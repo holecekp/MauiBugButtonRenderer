193b137b9b7d341090ae9c3d592996487a96990e
holecekp <holecekp@holecekp.com>
2022-10-04 05:55:42 +0200

    Add project files.

---
+++ MauiBugButtonRenderer/Platforms/Android/MainApplication.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace MauiBugButtonRenderer;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+		: base(handle, ownership)
+	{
+	}
+
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
